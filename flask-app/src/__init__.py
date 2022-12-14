a8378960acd96169ae7c96ba9af9cb0a0796ca3f
ana-bordeianu <116093824+ana-bordeianu@users.noreply.github.com>
2022-12-13 20:45:23 -0500

    Update __init__.py

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -25,13 +25,13 @@ def create_app():
     db.init_app(app)
     
     # Import the various routes
-    from src.views import views
-    from src.customers.customers import customers
-    from src.products.products  import products
+    from src.customer import customer
+    from src.employee import employee
+    from src.manager  import manager
 
     # Register the routes that we just imported so they can be properly handled
-    app.register_blueprint(views,       url_prefix='/classic')
-    app.register_blueprint(customers,   url_prefix='/classic')
-    app.register_blueprint(products,    url_prefix='/classic')
+    app.register_blueprint(customer,       url_prefix='/StoreSaver')
+    app.register_blueprint(employee,   url_prefix='/StoreSaver')
+    app.register_blueprint(manager,    url_prefix='/StoreSaver')
 
     return app
